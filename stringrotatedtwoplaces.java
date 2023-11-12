223c4015c5e2e1c86eda6fc59e8f63460dd88c28
SAGAR20-12 <surfersagar20@gmail.com>
2023-11-12 22:28:30 +0530

    Check if a string is rotated by two places

---
+++ stringrotatedtwoplaces.java
@@ -0,0 +1,10 @@
+public class stringrotatedtwoplaces {
+    public static boolean isRotated(String str1, String str2)
+    {
+        int n1 = str1.length();
+        int n2 = str2.length();
+        String clockwise = str1.substring(2)+str1.substring(0,2);
+        String anti = str1.substring(n1-2)+str1.substring(0,n1-2);
+        return (clockwise.equals(str2) || anti.equals(str2));
+    }
+    }
