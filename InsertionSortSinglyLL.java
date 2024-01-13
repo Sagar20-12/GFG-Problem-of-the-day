7ccc2b41bdae213859d36d0e29fefb3fccd836e1
SAGAR20-12 <surfersagar20@gmail.com>
2024-01-13 12:07:58 +0530

    Insertion sort in Singly LinkedList

---
+++ InsertionSortSinglyLL.java
@@ -0,0 +1,21 @@
+class InsertionSortSinglyLL {
+    public static Node insertionSort(Node head)
+    {
+        //code here
+        Node dummy = new Node(-1);
+        
+        while(head != null){
+            Node temp = dummy;
+            Node next = head.next;
+            
+            while(temp.next != null && temp.next.data < head.data){
+                temp = temp.next;
+            }
+            head.next = temp.next;
+            temp.next = head;
+            head = next;
+        }
+        
+        return dummy.next;
+    }
+}
