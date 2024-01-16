2332b78afc8192b7bc19a6af8501cf023ee85ed2
SAGAR20-12 <surfersagar20@gmail.com>
2024-01-16 13:39:53 +0530

    Dp approach using O(m x n) time Complexity

--- SequenceOfSequence.java
+++ SequenceOfSequence.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class SequenceOfSequence {
+/* class SequenceOfSequence {
     static int numberSequence(int m, int n)
     {
         // code here
@@ -13,4 +13,23 @@ class SequenceOfSequence {
     return number(m/2, n-1) + number(m-1, n);
     }
     
+} */
+class Solution{
+    static int numberSequence(int m, int n){
+        List<List<Integer>> dp = new ArrayList<>();
+        for (int i = 0; i < m + 1; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n + 1; j++) {
+                if (i == 0 || j == 0 || i < j) {
+                    row.add(0);
+                } else if (j == 1) {
+                    row.add(i);
+                } else {
+                    row.add(dp.get(i - 1).get(j) + dp.get(i / 2).get(j - 1));
+                }
+            }
+            dp.add(row);
+        }
+        return dp.get(m).get(n);
+    }
 }
