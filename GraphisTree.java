930f82ad8a62cf698c7effa8dc9b0636ce9d3a58
SAGAR20-12 <surfersagar20@gmail.com>
2024-01-24 19:10:49 +0530

    Check if a graph is tree or not

---
+++ GraphisTree.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class GraphisTree {
+    public boolean isTree(int n, int m, ArrayList<ArrayList<Integer>> edges) 
+    {
+        // code here
+        if(m!=n-1) return false;
+        int[] parent = new int[n];
+        Arrays.fill(parent, -1);
+        for(ArrayList<Integer> adj : edges){
+            int u = findParent(adj.get(0), parent);
+            int v = findParent(adj.get(1), parent);
+            if(u==v) return false;
+            parent[u] = v;
+            
+        } return true;
+    }
+    private static int findParent(int i, int[] parent){
+        while(parent[i]!=-1){
+            i = parent[i];
+        }
+        return i;
+    }
+    
+}
