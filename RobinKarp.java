ea6fb778d3b77e1dfe4d21ace516081f972e5afa
SAGAR20-12 <surfersagar20@gmail.com>
2024-03-06 23:41:55 +0530

    Robin Karp Algorithm

---
+++ RobinKarp.java
@@ -0,0 +1,18 @@
+public class RobinKarp {
+    ArrayList<Integer> search(String pattern, String text)
+    {
+        // your code here
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int a = pattern.length();
+        int b = text.length();
+        int count = 0;
+        for(int i = 0; i<b-a+1; i++){
+            String str1 = text.substring(i, i+a);
+            if(str1.equals(pattern)){
+                list.add(i+1);
+                count++;
+            }
+        }
+        return list;
+    }
+}
