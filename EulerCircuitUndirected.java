1f5f38753c5dcb9308509819a6b261824b95a5e2
SAGAR20-12 <surfersagar20@gmail.com>
2024-03-29 03:39:42 +0530

    Euler circuit for undirected Graph

---
+++ EulerCircuitUndirected.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class EulerCircuitUndirected {
+    public boolean isEularCircuitExist(int v, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        int deg = 0;
+        for(int i = 0; i<v; i++){
+            if(adj.get(i).size()%2 == 0){
+                deg++;
+            }
+        }
+        if(deg == v){
+            return true;
+        }
+        return false;
+    }
+}
